ed51922f9cd0c5c37c49d56d0e178f79a59bcbc5
Marc Davies <marcdaviesriot@gmail.com>
2019-07-04 01:13:48 -0700

    add props to customize error message

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Error404(){
+function Error404(props){
   return (
     <div>
       <style jsx>{`
@@ -9,7 +9,7 @@ function Error404(){
           color: white;
         }
       `}</style>
-      <h2>The page you are looking for does not exist!</h2>
+      <h2>The page {props.location.pathname} does not exist!</h2>
       <h3>Would you like to return <Link style={{ color: 'white' }} to="/">home</Link> instead?</h3>
     </div>
   );
