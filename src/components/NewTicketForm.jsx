4c7167fac095d1ab7ba9c7e3a43c66b782d2baf3
Darrion Gering and Marc Davies <darrion@>
2019-07-08 09:30:24 -0700

    add refs to form

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 function NewTicketForm(){
+  let _names = null;
+  let _location = null;
+  let _issue = null;
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
+  }
+
   return (
     <div>
       <style jsx>{`
@@ -10,22 +21,25 @@ function NewTicketForm(){
           margin-bottom: 5px;
         }
       `}</style>
-      <form>
+      <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/>
+          placeholder='Pair Names'
+          ref={(input) => {_names = input;}}/>
         <br/>
         <input
           type='text'
           id='location'
-          placeholder='Location'/>
+          placeholder='Location'
+          ref={(input) => {_location = input;}}/>
         <br/>
         <textarea
           rows="5"
           cols="80"
           id='issue'
-          placeholder='Describe your issue.'/>
+          placeholder='Describe your issue.'
+          ref={(input) => {_issue = input;}}/>
         <br/>
         <button type='submit'>Help!</button>
       </form>
